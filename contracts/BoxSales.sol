9f13f583bb81021d4a89b44b2669b65e47af93fc
maxeq <maxeq1@gmail.com>
2023-09-02 12:58:10 +0200

    x

--- contracts/BoxSales.sol
+++ contracts/BoxSales.sol
@@ -1,32 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "./GameToken.sol";
 import "./Avatars.sol";
 
-contract BoxSales {
+contract BoxSales is ERC721Enumerable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
     GameToken public gameToken;
     Avatars public avatars;
 
     address public owner;
 
     enum BoxType { Bronze, Silver, Gold }
-    mapping(BoxType => uint256) public boxPriceInWei;  // Set box prices in Wei
-    mapping(BoxType => uint256) public tokensRewarded; // Set tokens rewarded for buying a box
+    mapping(BoxType => uint256) public boxPriceInWei;
+    mapping(BoxType => uint256) public tokensRewarded;
 
-    event BoxPurchased(address indexed user, BoxType boxType, uint256 avatarId, uint256 tokenAmount);
+    event BoxPurchased(address indexed user, BoxType boxType);
+    event BoxUnboxed(address indexed user, uint256 avatarId, uint256 tokenAmount);
 
-    constructor(address _gameToken, address _avatars) {
+    constructor(address _gameToken, address _avatars) ERC721("BoxNFT", "BOX") {
         gameToken = GameToken(_gameToken);
         avatars = Avatars(_avatars);
         owner = msg.sender;
 
-        // Initial pricing and rewards
-        boxPriceInWei[BoxType.Bronze] = 0.01 ether; // just an example
+        boxPriceInWei[BoxType.Bronze] = 0.01 ether;
         boxPriceInWei[BoxType.Silver] = 0.05 ether; 
         boxPriceInWei[BoxType.Gold] = 0.1 ether;
 
-        tokensRewarded[BoxType.Bronze] = 10; // example token rewards
+        tokensRewarded[BoxType.Bronze] = 10;
         tokensRewarded[BoxType.Silver] = 50; 
         tokensRewarded[BoxType.Gold] = 100; 
     }
@@ -39,15 +44,25 @@ contract BoxSales {
     function buyBox(BoxType _boxType) public payable {
         require(msg.value == boxPriceInWei[_boxType], "Incorrect Ether sent");
 
-        // Reward tokens for purchase
-        uint256 tokenReward = tokensRewarded[_boxType];
-        gameToken.mint(msg.sender, tokenReward);
+        _mint(msg.sender, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
 
-        // Mint a new avatar based on box type
-        string memory boxName = _getBoxName(_boxType);
+        emit BoxPurchased(msg.sender, _boxType);
+    }
+
+    function unbox() public {
+        uint256 tokenId = _tokenIdOfBoxOwnedBy(msg.sender);
+        _burn(tokenId);
+
+        BoxType ownedBox = BoxType(tokenId % 3); // Simple way to determine box type from tokenId
+
+        string memory boxName = _getBoxName(ownedBox);
         uint256 avatarId = avatars.mintRandomAvatarFromBox(boxName);
 
-        emit BoxPurchased(msg.sender, _boxType, avatarId, tokenReward);
+        uint256 tokenReward = tokensRewarded[ownedBox];
+        gameToken.mint(msg.sender, tokenReward);
+
+        emit BoxUnboxed(msg.sender, avatarId, tokenReward);
     }
 
     function setBoxPrice(BoxType _boxType, uint256 _priceInWei) external onlyOwner {
@@ -62,5 +77,9 @@ contract BoxSales {
         if (_boxType == BoxType.Bronze) return "Bronze Box";
         if (_boxType == BoxType.Silver) return "Silver Box";
         return "Gold Box";
+    }
+
+    function _tokenIdOfBoxOwnedBy(address user) private view returns (uint256) {
+        return tokenOfOwnerByIndex(user, 0); // Assuming each user owns only one box at a time
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
 import dotenv from "dotenv";
 dotenv.config();
 
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!;
 const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY!;
+const POLYGON_SCAN_KEY = process.env.POLYGON_SCAN_KEY!;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
@@ -14,7 +16,22 @@ const config: HardhatUserConfig = {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [MUMBAI_PRIVATE_KEY]
     }
-  }
+  },
+etherscan: {
+  apiKey: {
+    mumbai: POLYGON_SCAN_KEY
+  },
+  customChains: [
+    {
+      network: "mumbai",
+      chainId: 80001,
+      urls: {
+        apiURL: "https://api-testnet.polygonscan.com/api",
+        browserURL: "https://mumbai.polygonscan.com/"
+      }
+    }
+  ]
+}
 };
 
 export default config;
